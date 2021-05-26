325d59b3e2fb3fdc46aa8eb7604f4f4192ce775d
predragf <predragku@gmail.com>
2021-05-26 12:23:50 +0200

    color icons hover added.

--- script/main.js
+++ script/main.js
@@ -1,5 +1,28 @@
 window.onload = function() {
 
+  sideLinks = document.getElementsByClassName("side-container")
+  console.log(sideLinks)
+
+  for (sideLink of sideLinks){
+    sideLink.addEventListener('mouseleave', function(){
+
+      bwImage = this.querySelectorAll('.bw-image')[0];
+      colorImage = this.querySelectorAll('.color-image')[0];
+
+      bwImage.classList.remove("hidden-image");
+      colorImage.classList.add("hidden-image");
+
+    })
+    sideLink.addEventListener('mouseenter', function(){
+
+      bwImage = this.querySelectorAll('.bw-image')[0];
+      colorImage = this.querySelectorAll('.color-image')[0];
+
+      colorImage.classList.remove("hidden-image");
+      bwImage.classList.add("hidden-image");
+    })
+  }
+
 };
 
 function sendEmail(){
